1bee9c899b56d60cfa62c849c874cd9c6fbb5310
Roman Khrabatyn <romankhr@yahoo.com>
2020-10-14 22:10:47 +0300

    added first project

---
+++ lesson-01/src/ua/lviv/lgs/Application.java
@@ -0,0 +1,124 @@
+package ua.lviv.lgs;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+//		int a=2;
+//		long b=3;
+//		System.out.println("var a= "+a);
+//		System.out.println("var b= "+b);
+
+		// додавання і віднімання
+//		int d=1;
+//		int e=2;
+//		int res1=d+e;
+//		int res2=d-e;
+//		int res3=d*e;
+//		float res4=d/e;
+//		
+//		
+//		
+//		System.out.println("var res1= "+res1);
+//		System.out.println("var res2= "+res2);
+//		System.out.println("var res3= "+res3);
+//		System.out.println("var res4= "+res4);
+
+//		приклад 2 інкремент/декремент
+//		int a=0;
+//		int b=++a;
+//		int c=a++;
+//		System.out.println("var a= "+a +"  b="+ b+ "  c"+c);
+//		
+
+		// приклад 3 присвоєння
+//		
+//		int f=1;
+//		int g=2;
+//		int h=3;
+//		f+=5;
+//		g*=4;
+//		h+=f*g;
+//		h%=6;
+//		System.out.println("var f= "+f);
+//		System.out.println("var g= "+g);
+//		System.out.println("var h= "+h);
+
+		// приклад 4. тренарні оператори
+
+//		int a=2;
+//		int r=a<0?-3:2;
+//		System.out.println("r="+r);
+
+		// приклад 5. логічні оператори
+//		boolean j=true;
+//		boolean k=false;
+//		boolean l=j|k;
+//		boolean m=j&k;
+//		boolean n=(!j&k)|(j&!k);
+//		boolean o=!j;
+//		System.out.println("var j= "+j);
+//		System.out.println("var k= "+k);
+//		System.out.println("var l= "+l);
+//		System.out.println("var m= "+m);
+//		System.out.println("var n= "+n);
+//		System.out.println("var o= "+o);
+
+		// приклад 6. порівняння
+
+//		int t=5;
+//		int u = 21;
+//		String result1 =(t>u)? "YES":"NO";
+//		String result2 =(t<u)? "YES":"NO";
+//		String result3 =(t==u)? "YES":"NO";
+//		System.out.println("res1 "+result1);
+//		System.out.println("res2 "+result2);
+//		System.out.println("res3 "+result3);
+
+		// приклад 7: конструкція if-else
+
+//		int a1 = 2;
+//		int b1 = 3;
+//		if (a1 < b1) {
+//System.out.println("a1>b1");
+//		}
+//		else {
+//			System.out.println("a1>b1");
+//		}
+		
+		// приклад 8 switch
+		
+//		int k1 = 4;
+//		switch (k1) {
+//		case 1:
+//		case 2:
+//		case 3: {
+//			System.out.println("1");
+//			break;
+//		}
+//		case 4:
+//			System.out.println("2");
+//			break;
+//		default:
+//			System.out.println("Default");
+//			break;
+//		}
+//		// приклад масиви
+		
+//		int  array1[] = new int[10];
+//		double [] array2 = new double[10];
+//		double [] array3 = {1.1, 2.2, 3.3,4.4,5.5};
+//		for (int i = 0; i < array1.length; i++) {
+//			array1[i]=i;
+//		}
+//			for (int i = 0; i < array1.length; i++) {
+//				System.out.println(array1[i]);
+//				
+//			}	
+				
+			//приклад Wrapperes  обгортки примітивів
+		
+		Integer a=2;
+		Double b=2.2;
+	}
+}
